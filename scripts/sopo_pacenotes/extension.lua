e2f10beae61ef63b4adf75f011a366229be25bf6
chrisraff <crharfifs@gmail.com>
2023-09-28 00:45:14 -0400

    bindable action and POC script

---
+++ scripts/sopo_pacenotes/extension.lua
@@ -0,0 +1,13 @@
+-- This Source Code Form is subject to the terms of the bCDDL, v. 1.1.
+-- If a copy of the bCDDL was not distributed with this
+-- file, You can obtain one at http://beamng.com/bCDDL-1.1.txt
+
+local M = {}
+
+local function test()
+    print('hello from pacenotes')
+end
+
+M.test = test
+
+return M
