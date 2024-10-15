fcc8a5ca11f57bb63d18068276cfc78dae34b41a
chrisraff <crharfifs@gmail.com>
2024-10-14 21:12:18 -0400

    go back to registerCoreModule, load at start

--- scripts/sopo_pacenotes/modScript.lua
+++ scripts/sopo_pacenotes/modScript.lua
@@ -2,4 +2,4 @@
 -- If a copy of the bCDDL was not distributed with this
 -- file, You can obtain one at http://beamng.com/bCDDL-1.1.txt
 
-load('scripts/sopo_pacenotes/extension')
+registerCoreModule('scripts/sopo_pacenotes/extension')
